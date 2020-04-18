bec39c3af42f6da6ce753731d99d080437f03e37
Achmad Fadhitya M <afadhitya@gmail.com>
2020-04-18 19:09:31 +0700

    add code

---
+++ src/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/getproducts", returnAllUsers).Methods("GET")
+	router.HandleFunc("/users", insertUsersMultipart).Methods("POST")
+	router.HandleFunc("/users", updateUserMultipart).Methods("PUT")
+	router.HandleFunc("/users", deleteUserData).Methods("DELETE")
+	http.Handle("/", router)
+	fmt.Println("Connected to port 1234")
+	log.Fatal(http.ListenAndServe(":1234", router))
+}
